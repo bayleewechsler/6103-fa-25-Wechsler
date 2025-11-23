4b2aa3c812ad3405112758ef4b93bb055fbbc191
bayleewechsler <baylee.wechsler@gwmail.gwu.edu>
2025-11-23 14:19:17 -0500

    load csvs

--- 6103 final project.py
+++ 6103 final project.py
@@ -1,5 +1,32 @@
 #6103 final project  
-#load data 
- 
+
+#import packages
 import numpy as np
 import pandas as pd
+import os 
+import zipfile
+import openpyxl
+
+library(reticulate)
+py_config()
+py_install("openpyxl")
+
+
+#set wd
+data_dir = "/Users/bayleewechsler/6103 Final Project"
+
+#load data
+
+def read_csv_safe(path):
+    try:
+        return pd.read_csv(path, encoding='utf-8')
+    except UnicodeDecodeError:
+        return pd.read_csv(path, encoding='cp1252')
+      
+year_end_prison = read_csv_safe(os.path.join(data_dir, "year-end-prison-2021.csv"))
+bjs_jail = read_csv_safe(os.path.join(data_dir, "BJS jail population overview.csv"))
+county_treatment_courts = read_csv_safe(os.path.join(data_dir, "County Treatment Court Count.csv"))
+incarceration_county = read_csv_safe(os.path.join(data_dir, "incarceration_trends_county.csv"))
+incarceration_state = read_csv_safe(os.path.join(data_dir, "incarceration_trends_state.csv"))
+treatment_facilities = pd.read_excel(os.path.join(data_dir, "FindTreament_Facility_listing_2025_11_05_245152.xlsx"), engine='openpyxl')
+
