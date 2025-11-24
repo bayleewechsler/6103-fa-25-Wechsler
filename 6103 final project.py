b9b898460f2bbdf61c47b7169f5e14c4114cb1d0
bayleewechsler <baylee.wechsler@gwmail.gwu.edu>
2025-11-24 16:42:21 -0500

    merge prep
    prepped first two datasets ahead of merge

--- 6103 final project.py
+++ 6103 final project.py
@@ -36,7 +36,7 @@ def read_csv_safe(file_like):
     except (UnicodeDecodeError, pd.errors.EmptyDataError):
         return None
 
-      
+#load csvs      
 year_end_prison = read_csv_safe(os.path.join(data_dir, "year-end-prison-2021.csv"))
 bjs_jail = read_csv_safe(os.path.join(data_dir, "BJS jail population overview.csv"))
 county_treatment_courts = read_csv_safe(os.path.join(data_dir, "County Treatment Court Count.csv"))
@@ -46,23 +46,6 @@ treatment_facilities = pd.read_excel(os.path.join(data_dir, "FindTreament_Facili
 
 #zip data is more tricky to load
 icpsr_zip = os.path.join(data_dir, "ICPSR_38048-V1.zip")  
-
-def load_zip_csv(zip_path):
-    dfs = {}
-    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
-        for file_name in zip_ref.namelist():
-            if not file_name.lower().endswith('.csv'):
-                continue
-            if file_name.startswith('__MACOSX') or file_name.startswith('._'):
-                continue
-            with zip_ref.open(file_name) as f:
-                df = read_csv_safe(f)
-                if df is not None:
-                    dfs[file_name] = df
-                    print(f"{file_name} loaded, shape: {df.shape}")
-                else:
-                    print(f"{file_name} is empty, skipped.")
-    return dfs
 
 def load_zip_tsv(zip_path):
     dfs = {}
@@ -114,10 +97,43 @@ sampled_acs_df = pd.concat(sampled_chunks)
 #go back and remove everything I added that isn't a necessary function/package 
 #and/or dataframe
 
+#do I have common columns for merges? no
+#bjs_jail merge-ready.
+bjs_column_titles= bjs_jail.iloc[11]
+bjs_jail= bjs_jail[12:]
+bjs_jail.columns= bjs_column_titles
+bjs_jail= bjs_jail.iloc[0:11]
+bjs_jail = bjs_jail.loc[:, ~bjs_jail.columns.isna()]
+#bjs_jail 'Year' column is 2013-2023
 
+#year_end_prison merge-ready.
+#need to add year column to year end prison to merge
+value_columns = [c for c in year_end_prison.columns 
+                 if "total_prison_pop_" in c]
+
+year_end_prison_long = year_end_prison.melt(
+    id_vars=["region", "state_name"],
+    value_vars=value_columns,
+    var_name="variable",
+    value_name="total_prison_pop")
+
+year_end_prison_long["Year"]= year_end_prison_long["variable"].str.extract(r"(\d{4})").astype(int)
+year_end_prison_long= year_end_prison_long.drop(columns=["variable"])
+print(year_end_prison_long.head())
+year_end_prison_long['region'] = year_end_prison_long['region'].fillna('NA')
+year_end_prison_long['state_name'] = year_end_prison_long['state_name'].fillna('NA')
+year_end_prison_long.isna().sum()
+
+#county_treatment_courts merge-ready
+county_treatment_courts["Year"]= 2023
+#"2024_County_Court_Count" is the same data but from 2024, not 2023
+
+#incarceration_county merge-ready
+#incarceration_state merge-ready
+#treatment_facilities merge-ready
+#icpsr_df merge-ready
 
 #merge all dataframes together (except acs) 
-#do I have common columns for merges?
 merge1= pd.merge(year_end_prison,bjs_jail, on="shared column name", how= "outer")
 merge2= pd.merge(merge1,county_treatment_courts, on="shared column name", how= "outer")
 merge3= pd.merge(merge2,incarceration_county, on="shared column name", how= "outer")
